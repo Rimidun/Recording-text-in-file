30819edcbbeced1b38376fc594c65fb823b9b850
x137@yandex.ru <1k2b3c4C>
2020-01-24 15:24:40 +0300

    all commit , part 2

---
+++ src/Reading.java
@@ -0,0 +1,23 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+
+public class Reading {
+    public static void main(String[] args) throws Exception {
+        Date date1 = new Date();
+        File file = new File("C:/Javarush/File2.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, 10000);
+        int i;
+        while((i=bufferedInputStream.read())!= -1){
+            System.out.print((char)i);
+        }
+        Date date2 = new Date();
+        System.out.println(" " + (date2.getTime() - date1.getTime()) + " мильсекунды");
+
+        fileInputStream.close();
+        bufferedInputStream.close();
+
+    }
+}
